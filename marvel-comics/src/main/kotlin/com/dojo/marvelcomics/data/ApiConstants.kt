76ce620733659be316337d9e137747893ee8a980
Miles Morales <pgsilva0698@gmail.com>
2021-05-01 00:29:24 -0300

    ignore credentials

--- marvel-comics/src/main/kotlin/com/dojo/marvelcomics/data/ApiConstants.kt
+++ marvel-comics/src/main/kotlin/com/dojo/marvelcomics/data/ApiConstants.kt
@@ -1,10 +1,6 @@
 package com.dojo.marvelcomics.data
 
 object ApiConstants {
-    const val MARVEL_API_URL = "https://gateway.marvel.com:443/"
-    const val MARVEL_API_KEY = "f860ec77b1d2f964ea121c232dee391a"
-    const val MARVEL_API_SECRET_KEY = "d2221e145e91cb750ad200accbb699a8f700f931"
-    const val MARVEL_API_HASH = "d17908908aa8e4ac0317bcb3fc6cd847"
-    const val MARVEL_API_TS = "1619837044622"
+    
 
 }
